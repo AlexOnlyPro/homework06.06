bd9cf1667776526bdfa9cc18887377c4f637d332
Alexey <birill56@gmail.com>
2023-06-02 15:07:41 +1000

    1

--- Program.cs
+++ Program.cs
@@ -1,23 +1,48 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// Задача 26 HARD Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 0 -> 1
+// 89,126 -> 5
+// 0,001->4
+// 000000 - > 1
 
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+System.Console.WriteLine("Введите число: ");
+double num = double.Parse(Console.ReadLine());
+int count = 1;
+int count2 = 1;
+// while (res1 * 10 % 10 != 0)
+// {   
+//     res1 = res1* 10;
+//     count++;
+// }
 
-// 6, 1, 33 -> [6, 1, 33]
+while (num * Math.Pow(10, 1 + count) % 10 != 0) { count++; };
 
-int [] array = new int [8];
 
-for (int i = 0; i < 8; i++)
-    if (i == 0) 
-    {
-        System.Console.WriteLine("Введите первое число массива: ");
-        array[i] = int.Parse(Console.ReadLine());
-    }
-    else
-    {
-        System.Console.WriteLine("Введите следующее число массива: ");
-        array[i] = int.Parse(Console.ReadLine());
-    }
-   
 
-for (int i = 0; i < 8; i++)
-    System.Console.Write(array[i] + " ");
+
+// int MyPow(int a,int n)
+// {
+//    result = 1;
+//    for(int i = 0;i < n; i++)
+//       result *= a;
+//    return result;
+// }
+// while (d * Math.Pow(10, 1 + i) % 10 != 0) { i++; }
+// while (num%10 > 10)
+// {
+//     num %= 10;
+//     count2++;
+// }
+
+// while (num >0)
+// {
+//     num = num/10;
+//     count2++;
+// }
+
+
+
+System.Console.WriteLine(count);
+System.Console.WriteLine(res1);
+// System.Console.WriteLine(count2);
+// System.Console.WriteLine(num);
